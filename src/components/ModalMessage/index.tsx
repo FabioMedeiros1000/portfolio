5034fc5524f36699e74eccaac80a2fe7839fcc54
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2025-01-07 14:30:06 -0300

    :bug: fix: fixing message sending

--- src/components/ModalMessage/index.tsx
+++ src/components/ModalMessage/index.tsx
@@ -27,45 +27,42 @@ const ModalMessage = forwardRef<ModalHandles, Props>(
     }))
 
     return (
-      <>
-        {isSendSuccess !== null && (
-          <Modal
-            ref={modalRef}
-            titleModal={
-              isSendSuccess
-                ? 'Mensagem enviada com sucesso!'
-                : 'Algo deu errado ao enviar a mensagem!'
-            }
-          >
-            <Container>
-              {isSendSuccess ? (
-                <p>
-                  <b>Mensagem enviada com sucesso!</b>
-                  <br />
-                  Fico muito feliz por você ter entrado em contato! Assim que
-                  possível, responderei diretamente no seu e-mail!
-                </p>
-              ) : (
-                <p>
-                  <b>
-                    Infelizmente, não conseguimos enviar sua mensagem neste
-                    momento!
-                  </b>
-                  <br />
-                  Isso pode ter ocorrido devido a um erro no servidor ou
-                  problemas de conexão. Por favor: Verifique se os campos do
-                  formulário estão preenchidos corretamente e tente enviar a
-                  mensagem novamente. Se o problema persistir, entre em contato
-                  diretamente através do e-mail:
-                  fabio.leandro.medeiros@gmail.com.
-                  <br />
-                  Obrigado pela paciência!
-                </p>
-              )}
-            </Container>
-          </Modal>
-        )}
-      </>
+      <Modal
+        ref={modalRef}
+        titleModal={
+          isSendSuccess === true
+            ? 'Mensagem enviada com sucesso!'
+            : isSendSuccess === false
+              ? 'Algo deu errado ao enviar a mensagem!'
+              : ''
+        }
+      >
+        <Container>
+          {isSendSuccess === true && (
+            <p>
+              <b>Mensagem enviada com sucesso!</b>
+              <br />
+              Fico muito feliz por você ter entrado em contato! Assim que
+              possível, responderei diretamente no seu e-mail!
+            </p>
+          )}
+          {isSendSuccess === false && (
+            <p>
+              <b>
+                Infelizmente, não conseguimos enviar sua mensagem neste momento!
+              </b>
+              <br />
+              Isso pode ter ocorrido devido a um erro no servidor ou problemas
+              de conexão. Por favor: Verifique se os campos do formulário estão
+              preenchidos corretamente e tente enviar a mensagem novamente. Se o
+              problema persistir, entre em contato diretamente através do
+              e-mail: fabio.leandro.medeiros@gmail.com.
+              <br />
+              Obrigado pela paciência!
+            </p>
+          )}
+        </Container>
+      </Modal>
     )
   }
 )
