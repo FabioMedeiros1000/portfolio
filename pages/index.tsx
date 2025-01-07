4eee63fd1d841411e221983bd755f39a5a574aef
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2025-01-07 19:19:14 -0300

    :recycle: refactor: moving app to next.js

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import About from '../src/components/About'
+import Contact from '../src/components/Contact'
+import Footer from '../src/components/Footer'
+import Header from '../src/components/Header'
+import Projects from '../src/components/Projects'
+import Resume from '../src/components/Resume'
+import Skills from '../src/components/Skills'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Resume />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Bem-vindo ao meu portfólio!</h1>
-      <p>Esta é a página inicial do meu site.</p>
-    </div>
-  );
-}
