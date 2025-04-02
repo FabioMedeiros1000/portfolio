ccd0637cb71b8726eb57cc2f845f838b741a30a6
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2025-04-02 15:52:18 -0300

    :bug: fix: fixing translation in hamburger menu

--- src/components/Hamburger/HamburgerMenu.tsx
+++ src/components/Hamburger/HamburgerMenu.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next'
+
 import { MenuContainer } from './styles'
 
 interface HamburgerMenuProps {
@@ -6,37 +8,39 @@ interface HamburgerMenuProps {
 }
 
 const HamburgerMenu = ({ isOpen, onClick }: HamburgerMenuProps) => {
+  const { t } = useTranslation()
+
   return (
     <MenuContainer isOpen={isOpen}>
       <ul>
         <li>
           <a href="#about" onClick={onClick}>
-            About
+            {t('header.about')}
           </a>
         </li>
         <li>
           <a href="#skills" onClick={onClick}>
-            Skills
+            {t('header.skills')}
           </a>
         </li>
         <li>
           <a href="#projects" onClick={onClick}>
-            Projects
+            {t('header.projects')}
           </a>
         </li>
         <li>
           <a href="#resume" onClick={onClick}>
-            Resume
+            {t('header.resume')}
           </a>
         </li>
         <li>
           <a href="#contact" onClick={onClick}>
-            Contact
+            {t('header.contact')}
           </a>
         </li>
         <li>
           <a href="#askMe" onClick={onClick}>
-            Ask Me
+            {t('header.askMe')}
           </a>
         </li>
       </ul>
